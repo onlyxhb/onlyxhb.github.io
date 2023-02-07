294ddab9fd93a295ce8b8181093a0d695b215237
徐辉波 <onlyxhb@qq.com>
2023-02-07 14:06:04 +0800

    feat: dumi-theme-chakra

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,14 +1,69 @@
-import { defineConfig } from 'dumi'
+import { defineConfig } from 'dumi';
+
+import { description, keywords, author } from './package.json';
+console.log('process.env.gh', process.env.gh);
+
+function withGH(uri: string): string {
+  const prefix = process.env.gh ? '/dumi-theme-chakra/' : '/';
+  return [prefix, uri].join('');
+}
 
 export default defineConfig({
-  favicons: ['https://yun.duiba.com.cn/80kziolo44.png'],
-  autoAlias: false,
-  outputPath: 'docs-dist',
+  // disable mfsu for HMR
+  mfsu: false,
+  base: withGH(''),
+  publicPath: withGH(''),
+  locales: [
+    { id: 'en-US', name: 'English' },
+    { id: 'zh-CN', name: '中文' }
+  ],
+  devtool: 'cheap-module-source-map',
+  sitemap: {
+    hostname: 'https://dumi-theme-chakra.deno.dev'
+  },
+  // all in one to fix ConfigProvider error
+  codeSplitting: {
+    jsStrategy: 'bigVendors'
+  },
+  apiParser: {},
+  resolve: {
+    entryFile: './src/index.ts'
+  },
+  alias: {
+    example: require.resolve('./src/index.ts')
+  },
+  clickToComponent: {},
+  // pass theme config
   themeConfig: {
-    rtl: true,
     name: '随波逐流',
     logo: 'https://yun.duiba.com.cn/80kziolo44.png',
-    footer: `Open-source MIT Licensed | Copyright © 2019-present<br />Powered by onlyxhb`
-  },
-  sitemap: { hostname: 'https://docs.onlystar.site' }
-})
+    helmetIcon: '🍺',
+    settingPanelVisible: true,
+    thumbBackground: true,
+    description,
+    keywords,
+    author,
+    social: {
+      github: {
+        name: 'dumi-theme-chakra',
+        link: 'https://github.com/innocces/dumi-theme-chakra'
+      },
+      discord: {
+        name: 'maryoku-ui-discord',
+        link: 'https://discord.gg/N82HK72uJk'
+      }
+    },
+    search: {
+      type: 'docsearch',
+      config: {
+        appId: 'CQNSFVVYJA',
+        apiKey: '2c50fefc041d570e018d5d9f569086b7',
+        indexName: 'dumi-theme-chakra'
+      }
+    },
+    hero: {
+      showVersionBadge: true
+    },
+    footer: `Copyright © ${new Date().getFullYear()} onlyxhb`
+  }
+});
